ccd93508f2e8111434682fd9f8d8822e7731b4dc
dineshkarthik-7 <23eg107f43@anurag.edu.in>
2025-08-07 15:38:58 +0530

    Updated name

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aashish reddy' });
+  res.render('index', { title: 'Dinesh' });
 });
 
 module.exports = router;
